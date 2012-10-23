7d729d45e31fc8af9bc54ba062b1ba11c6cb6448
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2012-10-23 11:49:47 -0200

    Testando

---
+++ Teste/src/test/java/com/guascoders/teste/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package com.guascoders.teste;
+
+import com.guascoders.teste.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
